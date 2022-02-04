39d72dd91194bd51d57b34a62bc1f9761470a418
Rodrigo Lopes <64791336+jrrodrigo421@users.noreply.github.com>
2022-02-04 11:07:10 -0300

    Janela WFM.py
    Usando a biblioteca PySimpleGUI podemos estar cirando um design grafico para melhor representação do codigo.
    Nesta biblio.. temos varios temas...

---
+++ JANELA WFM.py
@@ -0,0 +1,30 @@
+from PySimpleGUI import PySimpleGUI as sg
+import time
+import webbrowser
+
+sg.theme('Dark Red2')
+layout = [
+    [sg.Text('PREPARATION     '), sg.Input(key='PREPARATION')],
+    [sg.Text('IMPLEMENTATION'), sg.Input(key='IMPLEMENTATION')],
+    [sg.Text('VERIFICATION      '), sg.Input(key='VERIFICATION')],
+    [sg.Text('MONITORING        '), sg.Input(key='MONITORING')],
+    [sg.Text('COMPLETE          '), sg.Input(key='COMPLETE')],
+    [sg.Text('LINK WFM            '), sg.Input(key='LINK WFM')],
+    [sg.Checkbox('Salvar Dados?')],
+    [sg.Button('ENTRAR')]
+]
+
+janela = sg.Window('WFM', layout)
+
+
+Actual_Time = time.strftime("%I:%M:%S")
+
+
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'ENTRAR':
+        
+            print('DEU BOA')
